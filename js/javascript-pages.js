9bb7deae9e0d4551cc90d73890283e933955536d
Andy Kimbell <makimbell@gmail.com>
2019-02-28 20:01:34 -0500

    Fixed slider. Need to resize slider and add images.

--- js/javascript-pages.js
+++ js/javascript-pages.js
@@ -3,27 +3,27 @@ $(document).ready(function(){
 	var speed = 500; //transition speed - fade
 	var autoswitch = false; //auto slider options
 	var autoswitch_speed = 5000; //auto slider speed
-	
+
 	// add first initial active class
 	$(".slide").first().addClass("active");
-	
+
 	// hide all slides
-	$(".slide").hide;
-	
+	$(".slide").hide();
+
 	// show only active class slide
 	$(".active").show();
-	
+
 	// Next Event Handler
-	$('#next').on('click', nextSlide);// call function nextSlide
-	
+	$('#next').on('click', nextSlide);  // call function nextSlide
+
 	// Prev Event Handler
-	$('#prev').on('click', prevSlide);// call function prevSlide
-	
+	$('#prev').on('click', prevSlide);  // call function prevSlide
+
 	// Auto Slider Handler
 	if(autoswitch == true){
 		setInterval(nextSlide,autoswitch_speed);// call function and value 4000
 	}
-	
+
 	// Switch to next slide
 	function nextSlide(){
 		$('.active').removeClass('active').addClass('oldActive');
@@ -36,7 +36,7 @@ $(document).ready(function(){
 		$('.slide').fadeOut(speed);
 		$('.active').fadeIn(speed);
 	}
-	
+
 	// Switch to prev slide
 	function prevSlide(){
 		$('.active').removeClass('active').addClass('oldActive');
