e89246b0a8c169ae6dc1a5d8f039d6c2d1d94732
Andy Kimbell <makimbell@gmail.com>
2019-02-28 19:32:58 -0500

    Added bootstrap back in, lol. Added JQuery and a slider to beer page

---
+++ js/javascript-pages.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+	// options
+	var speed = 500; //transition speed - fade
+	var autoswitch = false; //auto slider options
+	var autoswitch_speed = 5000; //auto slider speed
+	
+	// add first initial active class
+	$(".slide").first().addClass("active");
+	
+	// hide all slides
+	$(".slide").hide;
+	
+	// show only active class slide
+	$(".active").show();
+	
+	// Next Event Handler
+	$('#next').on('click', nextSlide);// call function nextSlide
+	
+	// Prev Event Handler
+	$('#prev').on('click', prevSlide);// call function prevSlide
+	
+	// Auto Slider Handler
+	if(autoswitch == true){
+		setInterval(nextSlide,autoswitch_speed);// call function and value 4000
+	}
+	
+	// Switch to next slide
+	function nextSlide(){
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':last-child')){
+			$('.slide').first().addClass('active');
+		} else {
+			$('.oldActive').next().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
+	
+	// Switch to prev slide
+	function prevSlide(){
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':first-child')){
+			$('.slide').last().addClass('active');
+		} else {
+			$('.oldActive').prev().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
+});
